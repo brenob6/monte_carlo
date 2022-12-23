6ddf1cb6c8de8fdf47a592ffc5ca79f5c467dbc7
Breno Queiroz Lima <brenob6@gmail.com>
2022-12-22 23:07:35 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+function drawInSpace(callback) {
+	const canvas = document.getElementById("space")
+	const ctx = canvas.getContext("2d")
+	callback(ctx)
+}
+
+function drawRect() {
+
+	drawInSpace((ctx) => {
+
+		ctx.strokeRect(0, 0, 500, 500)
+	})
+}
+
+function drawCircle() {
+
+	drawInSpace((ctx) => {
+
+		ctx.beginPath();
+		ctx.arc(250, 250, 250, 0, Math.PI * 2, true);
+		
+		ctx.stroke();
+	})
+}
+
+function drawDot(x, y) {
+	drawInSpace((ctx) => {
+		r = Math.random() * 255
+		g = Math.random() * 255
+		b = Math.random() * 255
+
+		ctx.fillStyle = `rgba(${r},${g},${b}, 0.5)`
+		ctx.beginPath()
+		ctx.arc(x*500, y*500, 3, 0, Math.PI * 2, true);
+		ctx.fill()
+	})
+}
+
+function changePi(pi){
+	document.getElementById("pi").innerHTML = pi.toFixed(13)
+}
+
+drawCircle()
+drawRect()
+
+let count_square = 0;
+let count_circle = 0;
+
+setInterval(() => {
+	let xaxis = Math.random();
+	let yaxis = Math.random();
+
+	if(Math.pow(xaxis - 0.5, 2) + Math.pow(yaxis - 0.5, 2) < 0.25)
+		count_circle++;
+
+	count_square++;
+
+	drawDot(xaxis, yaxis)
+	pi = count_circle / ( count_square*0.25 );
+
+	changePi(pi)
+}, 500);
