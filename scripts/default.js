653f178969a6d7fcd86dfc0a306bb2df3bd62ded
Breno Queiroz Lima <brenob6@gmail.com>
2023-01-06 10:58:31 -0300

    better cards

--- scripts/default.js
+++ scripts/default.js
@@ -2,9 +2,9 @@ function createHeader() {
     let header = document.getElementsByTagName("header");
     header.item(0).innerHTML = 
        ` <nav>
-				<span class="logo">Math Labs</span>
+<span class="logo"><a href="../index.html">Math Labs</a></span>
 				<span class="icon">
-					<a href="https://www.geeksforgeeks.org/estimating-value-pi-using-monte-carlo/" 
+					<a href="#" 
             target="_blank">i</a>
 				</span>	
 				<span class="icon">?</span>
